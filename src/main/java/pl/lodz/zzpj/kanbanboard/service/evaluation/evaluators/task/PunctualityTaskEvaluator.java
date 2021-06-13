bc01500d6749ec12c65e0017637067ea34d833a1
FilipKowalski195 <42491472+FilipKowalski195@users.noreply.github.com>
2021-06-13 18:16:07 +0200

    Implements Tests PunctualityTaskEvaluator

--- src/main/java/pl/lodz/zzpj/kanbanboard/service/evaluation/evaluators/task/PunctualityTaskEvaluator.java
+++ src/main/java/pl/lodz/zzpj/kanbanboard/service/evaluation/evaluators/task/PunctualityTaskEvaluator.java
@@ -6,6 +6,8 @@ import pl.lodz.zzpj.kanbanboard.entity.TaskDetails.Difficulty;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class PunctualityTaskEvaluator implements TaskEvaluator {
 
     private final Map<Difficulty, Long> difficultyTimeEstimations;
@@ -16,6 +18,8 @@ public class PunctualityTaskEvaluator implements TaskEvaluator {
 
     @Override
     public double evaluate(Task task) {
+        checkNotNull(task.getClosedAt());
+
         var timespan = (double) ChronoUnit.DAYS.between(task.getCreatedAt(), task.getClosedAt());
         var estimation = (double) difficultyTimeEstimations.get(task.getDetails().getDifficulty());
 
---
+++ src/test/java/pl/lodz/zzpj/kanbanboard/service/evaluation/evaluators/task/PunctualityTaskEvaluatorTest.java
@@ -0,0 +1,95 @@
+package pl.lodz.zzpj.kanbanboard.service.evaluation.evaluators.task;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.lodz.zzpj.kanbanboard.entity.Task;
+import pl.lodz.zzpj.kanbanboard.entity.Task.Status;
+import pl.lodz.zzpj.kanbanboard.entity.TaskDetails;
+import pl.lodz.zzpj.kanbanboard.entity.TaskDetails.Difficulty;
+import pl.lodz.zzpj.kanbanboard.entity.User;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+class PunctualityTaskEvaluatorTest {
+
+    Map<Difficulty, Long> difficultyTimeEstimations = Map.of(
+            Difficulty.LOW, 2L,
+            Difficulty.MEDIUM, 4L,
+            Difficulty.HIGH, 6L
+    );
+
+    PunctualityTaskEvaluator evaluator;
+
+    User user = new User(
+            UUID.randomUUID(),
+            LocalDate.of(2020, 12, 12).atStartOfDay().toInstant(ZoneOffset.UTC),
+            "User@email.com",
+            "Filip",
+            "Kowalski",
+            "PASSW)RD123"
+    );
+
+    Instant getDate(int dayOfMonth) {
+        return LocalDate.of(2021, 6, 1 + dayOfMonth).atStartOfDay().toInstant(ZoneOffset.UTC);
+    }
+
+    TaskDetails taskDetailsEasy = new TaskDetails(
+            "Name", "Description", LocalDate.of(2021, 6, 12).atStartOfDay().toInstant(ZoneOffset.UTC), Difficulty.LOW);
+    TaskDetails taskDetailsMedium = new TaskDetails(
+            "Name", "Description", LocalDate.of(2021, 6, 12).atStartOfDay().toInstant(ZoneOffset.UTC),
+            Difficulty.MEDIUM);
+    TaskDetails taskDetailsHard = new TaskDetails(
+            "Name", "Description", LocalDate.of(2021, 6, 12).atStartOfDay().toInstant(ZoneOffset.UTC), Difficulty.HIGH);
+
+    Task easyLateTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(3), taskDetailsEasy);
+    Task easyInTimeTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(1), taskDetailsEasy);
+    Task mediumLateTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(6), taskDetailsMedium);
+    Task mediumInTimeTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(4), taskDetailsMedium);
+    Task hardLateTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(8), taskDetailsHard);
+    Task hardInTimeTask = new Task(UUID.randomUUID(), getDate(0), user, Status.DONE, getDate(5), taskDetailsHard);
+
+    Task notClosedTask = new Task(UUID.randomUUID(), getDate(0), user, Status.IN_PROGRESS, null, taskDetailsHard);
+
+    void init() {
+        evaluator = new PunctualityTaskEvaluator(difficultyTimeEstimations);
+    }
+
+    @Test
+    void evaluate_sameLevelTasks_LateTasksHaveLessPoints() {
+
+        init();
+
+        var actualEasyLateTask = evaluator.evaluate(easyLateTask);
+        var actualEasyInTimeTask = evaluator.evaluate(easyInTimeTask);
+
+        assertThat(actualEasyInTimeTask).isGreaterThan(actualEasyLateTask);
+
+        var actualMediumLateTask = evaluator.evaluate(mediumLateTask);
+        var actualMediumInTimeTask = evaluator.evaluate(mediumInTimeTask);
+
+        assertThat(actualMediumInTimeTask).isGreaterThan(actualMediumLateTask);
+
+        var actualHardLateTask = evaluator.evaluate(hardLateTask);
+        var actualHardInTimeTask = evaluator.evaluate(hardInTimeTask);
+
+        assertThat(actualHardInTimeTask).isGreaterThan(actualHardLateTask);
+    }
+
+    @Test
+    void evaluate_TaskIsNotClosed() {
+        init();
+
+        assertThatThrownBy(() -> evaluator.evaluate(notClosedTask))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+}
